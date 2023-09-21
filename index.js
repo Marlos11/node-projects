efd51977ef4ba6951d14605c8f61f93a2cfba1da
Marlos11 <marlos-gama@hotmail.com>
2023-09-21 18:05:21 -0300

    "feat: create api registre user

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
-const port = 3001
+const port = process.env.PORT ||3001
 app.use(express.json())
+app.use(cors())
 const uuid = require('uuid')
 
 const orders = []
